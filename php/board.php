f50581f7a47a99ccaf3168d41381c78fffc1f9a2
Joyce Chiang <107103035@nccu.edu.tw>
2022-03-29 20:37:43 +0800

    3/29 排行榜初版

--- php/board.php
+++ php/board.php
@@ -2,16 +2,18 @@
 session_start();
 include("../pdoInc.php");
 
+$return_arr = array();
 $sql = "SELECT * from Game ORDER BY score DESC LIMIT 3";
 $sth = $dbh->query($sql);
 while($row = $sth->fetch(PDO::FETCH_ASSOC)){
-    $arr = array($row['username'], $row['score']);
-    $arr = json_encode($arr);
-    echo $arr;
+    $row_array['username'] = $row['username'];
+    $row_array['score'] = $row['score'];
+
+    array_push($return_arr,$row_array);    
 };
-
-
-
+$return_arr = json_encode($return_arr); 
+echo $return_arr;
+unset($sth);
 
 
 
