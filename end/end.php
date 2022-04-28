b72bf6f5c912cd0e520c9eaf2152f78fbd8a2688
Joyce Chiang <107103035@nccu.edu.tw>
2022-04-28 16:57:42 +0800

    結局debug

--- end/end.php
+++ end/end.php
@@ -1,36 +1,36 @@
 <?php
 session_start();
 include("../pdoInc.php");
-include("../php/ending.php");
+// include("../php/ending.php");
 
-//加入離開時間
-// if(isset($_SESSION['username'])){
-//     $username = $_SESSION['username'];
-//     $sql = "UPDATE Game SET departure=now() WHERE username='$username'";
-//     $stmt= $dbh->prepare($sql);
-//         if($stmt->execute()){
-//         }
-//         unset($stmt);
-// }
+// 加入離開時間
+if(isset($_SESSION['username'])){
+    $username = $_SESSION['username'];
+    $sql = "UPDATE Game SET departure=now() WHERE username='$username'";
+    $stmt= $dbh->prepare($sql);
+        if($stmt->execute()){
+        }
+        unset($stmt);
+}
 
-// //取得離開時間與加入時間
-// $sql = "SELECT arrival, departure FROM Game WHERE username='$username'";
-// $sth = $dbh->query($sql);
-// while($row = $sth->fetch(PDO::FETCH_ASSOC)){
-//     $arrival = $row['arrival'];
-//     $departure = $row['departure'];
+//取得離開時間與加入時間
+$sql = "SELECT arrival, departure FROM Game WHERE username='$username'";
+$sth = $dbh->query($sql);
+while($row = $sth->fetch(PDO::FETCH_ASSOC)){
+    $arrival = $row['arrival'];
+    $departure = $row['departure'];
 
-//     $time_diff = strtotime($departure) - strtotime($arrival);
-// };
-// unset($sth);
+    $time_diff = strtotime($departure) - strtotime($arrival);
+};
+unset($sth);
 
-// //相減得到停留網站時間
-// $sql = "UPDATE Game SET stayTime=TIMESTAMPDIFF(SECOND, '$arrival', '$departure') WHERE username = '$username'";
-// $stmt= $dbh->prepare($sql);
-//     if($stmt->execute()){
-//     // echo "時間差";
-//     }
-// unset($stmt);
+//相減得到停留網站時間
+$sql = "UPDATE Game SET stayTime=TIMESTAMPDIFF(SECOND, '$arrival', '$departure') WHERE username = '$username'";
+$stmt= $dbh->prepare($sql);
+    if($stmt->execute()){
+    // echo "時間差";
+    }
+unset($stmt);
 ?>
 
 <!DOCTYPE html>
